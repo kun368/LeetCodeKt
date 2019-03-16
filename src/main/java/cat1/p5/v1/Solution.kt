af55d76fb7dd8fba57da6e4a7e044f71373acbe4
kun368 <kun368@126.com>
2019-03-16 23:45:49 +0800

    p7

---
+++ src/main/java/cat1/p5/v1/Solution.kt
@@ -0,0 +1,12 @@
+package cat1.p5.v1
+
+class Solution {
+    fun longestPalindrome(s: String): String {
+        return ""
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Solution().longestPalindrome("babad"))
+    println(Solution().longestPalindrome("cbbd"))
+}
