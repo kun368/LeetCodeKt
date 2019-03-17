fcde2eb864c717f611ac36ffb37108b37b48d2d7
kun368 <kun368@126.com>
2019-03-17 22:22:55 +0800

    p11

--- Readme.md
+++ Readme.md
@@ -2,3 +2,5 @@
 
 [力扣 (LeetCode)](https://leetcode-cn.com/) Kotlin实现
 
+- 包名按照题号排列：cat{题号/100} > p{题号}
+- 部分题目有多种解法版本，放在v1/v2/v3...下
--- src/main/java/cat001/p11/v1/Solution.kt
+++ src/main/java/cat001/p11/v1/Solution.kt
@@ -1,11 +1,26 @@
 package cat001.p11.v1
 
+import kotlin.math.absoluteValue
+
+/**
+ * O(N*log(N))解法
+ */
 class Solution {
     fun maxArea(height: IntArray): Int {
-       return 0
+        val idxs = height.indices.toSortedSet()
+        var maxSize = 0
+        height.withIndex().sortedBy { it.value } .forEach {
+            idxs.remove(it.index)
+            if (idxs.isNotEmpty()) {
+                val size1 = (idxs.first() - it.index).absoluteValue * it.value
+                val size2 = (idxs.last() - it.index).absoluteValue * it.value
+                maxSize = maxOf(maxSize, size1, size2)
+            }
+        }
+        return maxSize
     }
 }
 
 fun main(args: Array<String>) {
-
+    println(Solution().maxArea(intArrayOf(1,8,6,2,5,4,8,3,7)))
 }
