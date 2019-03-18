3605aa4c0a7bdea20174e4a556a75cad1405adc4
kunwu <zhengkun.zzk@alibaba-inc.com>
2019-03-18 21:07:59 +0800

    p19

---
+++ src/main/java/cat001/p19/v1/Solution.kt
@@ -0,0 +1,31 @@
+package cat001.p19.v1
+
+import utils.ListNode
+
+class Solution {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        if (head == null) {
+            return null
+        }
+
+        fun dfs(cur: ListNode?, pre: ListNode): Int {
+            if (cur == null) {
+                return 0
+            }
+            val rank = dfs(cur.next, cur) + 1
+            if (rank == n) {
+                pre.next = cur.next
+            }
+            return rank
+        }
+
+        val mock = ListNode(-1)
+        mock.next = head
+        dfs(mock, head)
+        return mock.next
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Solution().removeNthFromEnd(ListNode.parse("1->2->3->4->5"), 2))
+}
