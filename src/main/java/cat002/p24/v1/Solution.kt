dce09176abd40f1b36cb909ec819139e7a356eef
kunwu <zhengkun.zzk@alibaba-inc.com>
2019-03-19 19:34:45 +0800

    p24-25

---
+++ src/main/java/cat002/p24/v1/Solution.kt
@@ -0,0 +1,28 @@
+package cat002.p24.v1
+
+import utils.ListNode
+
+class Solution {
+    fun swapPairs(head: ListNode?): ListNode? {
+        val mock = ListNode(-1)
+        mock.next = head
+
+        fun dfs(pre: ListNode, a: ListNode?, b: ListNode?) {
+            if (a == null || b == null) {
+                return
+            }
+            val next = b.next
+            pre.next = b
+            b.next = a
+            a.next = next
+            dfs(a, a.next, a.next?.next)
+        }
+
+        dfs(mock, mock.next, mock.next?.next)
+        return mock.next
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Solution().swapPairs(ListNode.parse("1->2->3->4")))
+}
