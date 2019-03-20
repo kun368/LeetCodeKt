c3ea0267f6d222a4812eedc4c3ba1de316107eef
kunwu <zhengkun.zzk@alibaba-inc.com>
2019-03-20 21:53:13 +0800

    up

--- src/main/java/cat002/p29/v3/Solution.kt
+++ src/main/java/cat002/p29/v3/Solution.kt
@@ -1,8 +1,31 @@
 package cat002.p29.v3
 
 class Solution {
+
+    private val L = -(1L.shl(31))
+    private val R = 1L.shl(31) - 1
+
     fun divide(dividend: Int, divisor: Int): Int {
-        // TODO 待解决
-        return 0
+        var a = dividend.toBigInteger().abs()
+        val b = divisor.toBigInteger().abs()
+        var ret: Long = 0
+        for (i in 36 downTo 0) {
+            if (a >= b.shl(i)) {
+                a -= b.shl(i)
+                ret += 1L.shl(i)
+            }
+        }
+        ret = if ((dividend >= 0 && divisor >= 0) || (dividend < 0 && divisor < 0)) {
+            ret
+        } else {
+            -ret
+        }
+        return if (ret < L || ret > R) R.toInt() else ret.toInt()
     }
+}
+
+fun main(args: Array<String>) {
+    println(Solution().divide(10, 3))
+    println(Solution().divide(7, -3))
+    println(Solution().divide(-2147483648, -1))
 }
