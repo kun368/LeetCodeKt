b0f25c6cf4f426ce6d3b98d2a57d404265b398d6
kun368 <kun368@126.com>
2019-04-08 22:13:51 +0800

    好多水题

---
+++ src/main/java/cat007/p78/v1/Solution.kt
@@ -0,0 +1,11 @@
+package cat007.p78.v1
+
+class Solution {
+    fun subsets(nums: IntArray): List<List<Int>> {
+        return (0 until 1.shl(nums.size)).map { i -> nums.filterIndexed { idx, _ -> i.and(1.shl(idx)) != 0 } }
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Solution().subsets(intArrayOf(1, 2, 3)))
+}
---
+++ src/main/java/cat008/p88/v1/Solution.kt
@@ -0,0 +1,7 @@
+package cat008.p88.v1
+
+class Solution {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        
+    }
+}
