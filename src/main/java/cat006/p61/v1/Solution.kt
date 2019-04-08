4d526d2a46e822c3f9c8de581dbedf2d27cb9e36
kunwu <zhengkun.zzk@alibaba-inc.com>
2019-04-08 18:05:25 +0800

    code

--- src/main/java/cat006/p61/v1/Solution.kt
+++ src/main/java/cat006/p61/v1/Solution.kt
@@ -7,31 +7,33 @@ class Solution {
         if (head == null || k == 0) {
             return head
         }
+        val mock = ListNode(-1)
+        mock.next = head
+        var totalCount = 0
 
-        var last: ListNode? = null
-        fun dfs(pre: ListNode, cur: ListNode?): Int {
+        fun dfs(pre: ListNode, cur: ListNode?, has: Int): Int {
             if (cur == null) {
-                last = pre
+                if (k % has != 0) {
+                    pre.next = mock.next
+                }
+                totalCount = has
                 return 0
             }
-            val dep = dfs(cur, cur.next) + 1
-            if (dep == k) {
+            val dep = dfs(cur, cur.next, has + 1) + 1
+            if (dep == k % totalCount) {
                 pre.next = null
+                mock.next = cur
             }
             return dep
         }
-
-        val mock = ListNode(-1)
-        mock.next = head
-        dfs(mock, mock.next)
-        last!!.next = mock.next
+        dfs(mock, mock.next, 0)
         return mock.next
     }
 }
 
 
-// TODO solve
 fun main(args: Array<String>) {
-    println(Solution().rotateRight(ListNode.parse("1->2->3->4->5"), 2))
+    println(Solution().rotateRight(ListNode.parse("1->2->3->4->5"), 0))
     println(Solution().rotateRight(ListNode.parse("0->1->2"), 4))
+    println(Solution().rotateRight(ListNode.parse("1"), 1))
 }
