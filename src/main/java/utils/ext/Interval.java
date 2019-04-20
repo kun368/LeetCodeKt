e8dfaad16ab149322658f1dd717818a3e1653f3b
kun368 <kun368@126.com>
2019-04-20 13:13:14 +0800

    编译不通过？？？

---
+++ src/main/java/utils/ext/Interval.java
@@ -0,0 +1,25 @@
+package utils.ext;
+
+public class Interval {
+
+    public int start;
+    public int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+
+    @Override
+    public String toString() {
+        return "Interval{" +
+                "start=" + start +
+                ", end=" + end +
+                '}';
+    }
+}
