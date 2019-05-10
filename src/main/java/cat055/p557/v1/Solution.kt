e37d8ee597e4798038d522b5fc66f8f71ebb7609
kun368 <kun368@126.com>
2019-05-10 08:57:05 +0800

    1

---
+++ src/main/java/cat055/p557/v1/Solution.kt
@@ -0,0 +1,12 @@
+package cat055.p557.v1
+
+/**
+ * 反转字符串中的单词 III
+ */
+class Solution {
+    fun reverseWords(s: String): String = s.split(' ').joinToString(" ") { it.reversed() }
+}
+
+fun main(args: Array<String>) {
+    println(Solution().reverseWords("Let's take LeetCode contest"))
+}
