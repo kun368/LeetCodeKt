de47a625b3bb960015e42f0fcf7d05fce055781e
kun368 <kun368@126.com>
2019-05-10 22:20:22 +0800

    水题

---
+++ src/main/java/cat023/p238/v1/Solution.kt
@@ -0,0 +1,20 @@
+package cat023.p238.v1
+
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val ret = IntArray(nums.size) { 1 }
+        for (i in 1 until nums.size) {
+            ret[i] = ret[i - 1] * nums[i - 1]
+        }
+        var mul = 1
+        for (i in nums.lastIndex downTo 0) {
+            ret[i] *= mul
+            mul *= nums[i]
+        }
+        return ret
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Solution().productExceptSelf(intArrayOf(1, 2, 3, 4)).toList())
+}
