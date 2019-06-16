6f477f2ffef922435798dc2ec5f374eb6c911ca6
kun368 <kun368@126.com>
2019-06-16 10:30:59 +0800

    up

---
+++ src/main/java/cat018/p184/v1/main.sql
@@ -0,0 +1,41 @@
+drop table if exists Employee;
+drop table if exists Department;
+Create table If Not Exists Employee
+(
+    Id           int,
+    Name         varchar(255),
+    Salary       int,
+    DepartmentId int
+);
+Create table If Not Exists Department
+(
+    Id   int,
+    Name varchar(255)
+);
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('1', 'Joe', '70000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('2', 'Jim', '90000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('3', 'Henry', '80000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('4', 'Sam', '60000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('5', 'Max', '90000', '1');
+insert into Department (Id, Name)
+values ('1', 'IT');
+insert into Department (Id, Name)
+values ('2', 'Sales');
+
+
+--------------------------------------------------------------------------
+
+select a.Name as Department, b.Name as Employee, b.Salary as Salary
+from Department a
+         left outer join Employee b
+                         on a.Id = b.DepartmentId
+         join (select DepartmentId, max(Salary) as max_salary
+               from Employee
+               group by DepartmentId) c
+              on b.DepartmentId = c.DepartmentId and b.Salary = c.max_salary
+;
---
+++ src/main/java/cat018/p185/v1/main.sql
@@ -0,0 +1,42 @@
+drop table if exists Employee;
+drop table if exists Department;
+Create table If Not Exists Employee
+(
+    Id           int,
+    Name         varchar(255),
+    Salary       int,
+    DepartmentId int
+);
+Create table If Not Exists Department
+(
+    Id   int,
+    Name varchar(255)
+);
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('1', 'Joe', '85000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('2', 'Henry', '80000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('3', 'Sam', '60000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('4', 'Max', '90000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('5', 'Janet', '69000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('6', 'Randy', '85000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('7', 'Will', '70000', '1');
+insert into Department (Id, Name)
+values ('1', 'IT');
+insert into Department (Id, Name)
+values ('2', 'Sales');
+
+----------------------------------------------------------------------------
+
+select *
+from Employee;
+select *
+from Department;
+
+----------------------------------------------------------------------------
+
