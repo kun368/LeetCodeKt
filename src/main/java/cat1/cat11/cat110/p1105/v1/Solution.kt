38e38b1e785d911ac9f83edddad070a51a190a02
kun368 <kun368@126.com>
2019-06-30 12:26:25 +0800

    周赛四道题

---
+++ src/main/java/cat1/cat11/cat110/p1105/v1/Solution.kt
@@ -0,0 +1,39 @@
+package cat1.cat11.cat110.p1105.v1
+
+class Solution {
+    fun gen(maxn: Int): List<Int> {
+        val arr = ArrayList<Int>()
+        var cnt = 1
+        var max = 0
+        var rev = false
+        while (max < maxn) {
+            val span = (max + 1) until (max + 1 + cnt)
+            arr.addAll(if (rev) span.reversed().toList() else span.toList())
+            rev = !rev
+            max = span.last
+            cnt *= 2
+        }
+        return arr
+    }
+
+    fun pathInZigZagTree(label: Int): List<Int> {
+        val gen = gen(label)
+        val ret = ArrayList<Int>()
+        var idx = gen.indexOf(label)
+        while (true) {
+            ret.add(gen[idx])
+            if (idx ==0) {
+                break
+            }
+            idx = (idx - 1) / 2
+        }
+        return ret.reversed()
+    }
+}
+
+fun main() {
+    println(Solution().pathInZigZagTree(14))
+    println(Solution().pathInZigZagTree(26))
+    println(Solution().pathInZigZagTree(1))
+    println(Solution().pathInZigZagTree(1000000))
+}
---
+++ src/main/java/cat1/cat11/cat110/p1106/v1/Solution.kt
@@ -0,0 +1,36 @@
+package cat1.cat11.cat110.p1106.v1
+
+class Solution {
+    fun minHeightShelves(books: Array<IntArray>, shelf_width: Int): Int {
+        val dp = Array(books.size) { Int.MAX_VALUE / 2 }
+        dp[0] = books[0][1]
+        for (i in 1 until books.size) {
+            var kuan = 0
+            var maxh = 0
+            for (j in i downTo 0) {
+                kuan += books[j][0]
+                if (kuan > shelf_width) {
+                    break
+                }
+                maxh = maxOf(maxh, books[j][1])
+                dp[i] = minOf(dp[i], dp.getOrElse(j - 1) { 0 } + maxh)
+            }
+        }
+        return dp.last()
+    }
+}
+
+fun main() {
+    println(Solution().minHeightShelves(arrayOf(
+            intArrayOf(1, 1),
+            intArrayOf(2, 3),
+            intArrayOf(2, 3),
+            intArrayOf(1, 1),
+            intArrayOf(1, 1),
+            intArrayOf(1, 1),
+            intArrayOf(1, 2)
+    ), 4))
+    println(Solution().minHeightShelves(arrayOf(
+            intArrayOf(1, 1)
+    ), 1))
+}
