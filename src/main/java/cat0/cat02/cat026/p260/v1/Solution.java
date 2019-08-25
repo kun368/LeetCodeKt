8ec2857ff3cfc61a5cd93cea67b8abfb5ae08eee
kun368 <kun368@126.com>
2019-08-25 19:58:31 +0800

    add

---
+++ src/main/java/cat0/cat02/cat026/p260/v1/Solution.java
@@ -0,0 +1,27 @@
+package cat0.cat02.cat026.p260.v1;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int res = 0;
+        for (int i : nums) {
+            res ^= i;
+        }
+        int n = res & (~res + 1);
+        int a = 0, b = 0;
+        for (int i : nums) {
+            if ((i & n) > 0) {
+                a ^= i;
+            } else {
+                b ^= i;
+            }
+        }
+        return new int[]{a, b};
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new Solution().singleNumber(new int[]{1, 2, 1, 3, 2, 5});
+        System.out.println(Arrays.toString(ints));
+    }
+}
