478ae531e0519c9b2d1954d9f0f3cd40c27dcdcc
kunwu <zhengkun.zzk@alibaba-inc.com>
2019-12-11 15:49:50 +0800

    先通过

---
+++ src/main/java/cat0/cat00/cat003/p30/v1/Main.java
@@ -0,0 +1,54 @@
+package cat0.cat00.cat003.p30.v1;
+
+import java.util.*;
+
+import static utils.java.ArrayInputUtils.parseStrArr1;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        if (s.length() == 0 || words.length == 0) {
+            return Collections.emptyList();
+        }
+        int N = words.length, M = words[0].length();
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        }
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < M; ++i) {
+            Queue<String> qu = new ArrayDeque<>();
+            HashMap<String, Integer> curCnt = new HashMap<>();
+            for (int j = i; j + M <= s.length(); j += M) {
+                String s1 = s.substring(j, j + M);
+                qu.add(s1);
+                curCnt.put(s1, curCnt.getOrDefault(s1, 0) + 1);
+                if (qu.size() > N) {
+                    String top = qu.poll();
+                    Integer preCnt = curCnt.get(top);
+                    if (preCnt == 1) {
+                        curCnt.remove(top);
+                    } else {
+                        curCnt.put(top, preCnt - 1);
+                    }
+                }
+                if (qu.size() == N && curCnt.equals(cnt)) {
+                    ret.add(j + M - N * M);
+                }
+            }
+        }
+        return ret;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().findSubstring("barfoothefoobarman",
+                parseStrArr1("[\"foo\",\"bar\"]")));
+        System.out.println(new Solution().findSubstring("wordgoodgoodgoodbestword",
+                parseStrArr1("[\"word\",\"good\",\"best\",\"word\"]")));
+        System.out.println(new Solution().findSubstring("barfoofoobarthefoobarman",
+                parseStrArr1("[\"bar\",\"foo\",\"the\"]")));
+        System.out.println(new Solution().findSubstring("wordgoodgoodgoodbestword",
+                parseStrArr1("[\"word\",\"good\",\"best\",\"good\"]")));
+    }
+}
---
+++ src/main/java/utils/java/ArrayInputUtils.java
@@ -0,0 +1,76 @@
+package utils.java;
+
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.ArrayUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 解析数组输入方便Utils
+ */
+public class ArrayInputUtils {
+
+    // ========================== 一维数组 ==========================
+
+    private static List<String> parseStrList1(String str) {
+        try {
+            return JSON.parseArray(str, String.class);
+        } catch (Exception e) {
+            return Convert.toList(String.class, str);
+        }
+    }
+
+    public static String[] parseStrArr1(String str) {
+        return Convert.toStrArray(parseStrList1(str));
+    }
+
+    public static int[] parseIntArr1(String str) {
+        return ArrayUtil.unWrap(Convert.toIntArray(parseStrList1(str)));
+    }
+
+    public static long[] parseLongArr1(String str) {
+        return ArrayUtil.unWrap(Convert.toLongArray(parseStrList1(str)));
+    }
+
+    // ========================== 二维数组 ==========================
+
+    private static List<List<String>> parseStrList2(String str) {
+        List<List<String>> ret = new ArrayList<>();
+        JSONArray jsonArray = JSON.parseArray(str);
+        for (int i = 0; i < jsonArray.size(); ++i) {
+            ret.add(jsonArray.getJSONArray(i).toJavaList(String.class));
+        }
+        return ret;
+    }
+
+    public static int[][] parseIntArr2(String str) {
+        List<List<String>> lists = parseStrList2(str);
+        int[][] ret = new int[lists.size()][];
+        for (int i = 0; i < lists.size(); ++i) {
+            ret[i] = ArrayUtil.unWrap(Convert.toIntArray(lists.get(i)));
+        }
+        return ret;
+    }
+
+    public static long[][] parseLongArr2(String str) {
+        List<List<String>> lists = parseStrList2(str);
+        long[][] ret = new long[lists.size()][];
+        for (int i = 0; i < lists.size(); ++i) {
+            ret[i] = ArrayUtil.unWrap(Convert.toLongArray(lists.get(i)));
+        }
+        return ret;
+    }
+
+    // ========================== 测试 ==========================
+
+    public static void main(String[] args) {
+        System.out.println(parseStrList1("1, 2"));
+        System.out.println(parseStrList1("[1, 2]"));
+        System.out.println(parseStrList2("[[1, 2], [3, 4]]"));
+        System.out.println(Arrays.deepToString(parseIntArr2("[[1, 2], [3, 4]]")));
+    }
+}
