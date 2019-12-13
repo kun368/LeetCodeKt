921387a2d09697ef41a34f29cc42e69f99f162f6
kunwu <zhengkun.zzk@alibaba-inc.com>
2019-12-13 11:32:10 +0800

    dp+贪心

---
+++ src/main/java/cat0/cat00/cat004/p45/v1/Main.java
@@ -0,0 +1,24 @@
+package cat0.cat00.cat004.p45.v1;
+
+import utils.java.ArrayInputUtils;
+
+class Solution {
+    public int jump(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[nums.length - 1] = 0;
+        for (int i = nums.length - 2; i >= 0; --i) {
+            dp[i] = Integer.MAX_VALUE / 2;
+            for (int j = i + 1; j < nums.length && j <= i + nums[i]; ++j) {
+                dp[i] = Math.min(dp[i], dp[j] + 1);
+            }
+        }
+        return dp[0];
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().jump(ArrayInputUtils.parseIntArr1("[2,3,1,1,4]")));
+        System.out.println(new Solution().jump(ArrayInputUtils.parseIntArr1("[2,3,0,1,4]")));
+    }
+}
---
+++ src/main/java/cat0/cat00/cat004/p45/v2/Main.java
@@ -0,0 +1,27 @@
+package cat0.cat00.cat004.p45.v2;
+
+import utils.java.ArrayInputUtils;
+
+class Solution {
+    public int jump(int[] nums) {
+        int cnt = 0;
+        int l = 0, r = 0;
+        while (r < nums.length - 1) {
+            int max = r;
+            for (int i = l; i <= r; ++i) {
+                max = Math.max(max, i + nums[i]);
+            }
+            cnt += 1;
+            l = r + 1;
+            r = max;
+        }
+        return cnt;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().jump(ArrayInputUtils.parseIntArr1("[2,3,1,1,4]")));
+        System.out.println(new Solution().jump(ArrayInputUtils.parseIntArr1("[2,3,0,1,4]")));
+    }
+}
