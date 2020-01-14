a3c75ef55b88fe3c3dfb4834e6d451db6230eeff
kunwu <zhengkun.zzk@alibaba-inc.com>
2020-01-14 19:04:18 +0800

    更新

--- src/main/java/match/weekly171/p4/Main.java
+++ src/main/java/match/weekly171/p4/Main.java
@@ -1,51 +1,36 @@
 package match.weekly171.p4;
 
 
+import java.util.Arrays;
+
 class Solution {
 
     private static final int MAX = 0x3f3f3f3f;
 
-    private int dis(String s, int x, int y) {
-        if (x == 0) {
-            return 0;
-        }
-        int a = s.charAt(x - 1) - 'A';
-        int b = s.charAt(y - 1) - 'A';
-        int ax = a / 6, ay = a % 6;
-        int bx = b / 6, by = b % 6;
-        // System.out.println("dis, x = " + (char) (a + 'A') + ", y = " + (char) (b + 'A') + ", ret = " + (Math.abs(ax - bx) + Math.abs(ay - by)));
-        return Math.abs(ax - bx) + Math.abs(ay - by);
+    private int dis(int x, int y) {
+        return Math.abs((x / 6) - (y / 6)) + Math.abs((x % 6) - (y % 6));
     }
 
     public int minimumDistance(String word) {
         int N = word.length();
-        int[] psum = new int[N + 1];
+        int[][][] dp = new int[N + 1][27][27];
         for (int i = 1; i <= N; ++i) {
-            psum[i] = psum[i - 1] + dis(word, i - 1, i);
-        }
-        int[][] dp = new int[N + 1][N + 1];
-        for (int i = 0; i <= N; ++i) {  // 0
-            for (int j = 0; j <= N; ++j) { // 1
-                if (i == j) {
-                    dp[i][j] = MAX;
-                } else if (i == 0 || j == 0) {
-                    dp[i][j] = psum[Math.max(i, j)];
-                } else {
-                    dp[i][j] = MAX;
-                    int end = Math.min(i, j) - 1;
-                    int add = psum[Math.max(i, j)] - psum[end + 2];
-                    for (int k = 0; k < end; ++k) {
-                        dp[i][j] = Math.min(dp[i][j], dp[end][k] + dis(word, end, i) + dis(word, k, end + 2) + add);
-                        dp[i][j] = Math.min(dp[i][j], dp[k][end] + dis(word, k, i) + dis(word, end, end + 2) + add);
-                    }
-                }
-                 System.out.println("i = " + i + ", j = " + j + ", ans = " + dp[i][j]);
+            for (int j = 0; j < dp[i].length; ++j) {
+                Arrays.fill(dp[i][j], MAX);
             }
         }
         int ans = MAX;
-        for (int k = 0; k < N; ++k) {
-            ans = Math.min(ans, dp[N][k]);
-            ans = Math.min(ans, dp[k][N]);
+        for (int i = 1; i <= N; ++i) {
+            int cur = word.charAt(i - 1) - 'A';
+            for (int j = 0; j < 26; ++j) {
+                for (int k = 0; k < 26; ++k) {
+                    dp[i][cur][k] = Math.min(dp[i][cur][k], dp[i-1][j][k] + dis(j, cur));
+                    dp[i][j][cur] = Math.min(dp[i][j][cur], dp[i-1][j][k] + dis(k, cur));
+                    if (i == N) {
+                        ans = Math.min(ans, Math.min(dp[i][cur][k], dp[i][j][cur]));
+                    }
+                }
+            }
         }
         return ans;
     }
@@ -53,9 +38,9 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(new Solution().minimumDistance("CAKE"));
+        System.out.println(new Solution().minimumDistance("CAKE"));
         System.out.println(new Solution().minimumDistance("HAPPY"));
-//        System.out.println(new Solution().minimumDistance("NEW"));
-//        System.out.println(new Solution().minimumDistance("YEAR"));
+        System.out.println(new Solution().minimumDistance("NEW"));
+        System.out.println(new Solution().minimumDistance("YEAR"));
     }
 }
