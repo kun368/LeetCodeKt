4e7ecc8c5a6cbc2806dc4c6aa730666184ff81df
kun368 <kun368@126.com>
2020-03-01 12:01:36 +0800

    解析树

--- src/main/java/utils/java/TreeNode.java
+++ src/main/java/utils/java/TreeNode.java
@@ -1,5 +1,13 @@
 package utils.java;
 
+import cn.hutool.core.convert.Convert;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONType;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+import java.util.*;
+
+@JSONType(orders = {"val", "left", "right"})
 public class TreeNode {
 
     public int val;
@@ -16,12 +24,43 @@ public class TreeNode {
         this.right = right;
     }
 
+    /**
+     * 解析二叉树
+     * 例如：[1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+     */
+    public static TreeNode parse(String str) {
+        Integer[] arr = Convert.toIntArray(DebugUtils.parseStrList1(str));
+        if (arr == null || arr.length == 0 || arr[0] == null) {
+            return null;
+        }
+        int idx = 0;
+        TreeNode root = new TreeNode(arr[idx++]);
+        List<TreeNode> roots = Collections.singletonList(root);
+        while (idx < arr.length) {
+            List<TreeNode> nextRoots = new ArrayList<>();
+            for (TreeNode i : roots) {
+                Integer cur = arr[idx++];
+                if (cur != null) {
+                    i.left = new TreeNode(cur);
+                    nextRoots.add(i.left);
+                }
+                cur = arr[idx++];
+                if (cur != null) {
+                    i.right = new TreeNode(cur);
+                    nextRoots.add(i.right);
+                }
+            }
+            roots = nextRoots;
+        }
+        return root;
+    }
+
     @Override
     public String toString() {
-        return "TreeNode{" +
-                "val=" + val +
-                ", left=" + left +
-                ", right=" + right +
-                '}';
+        return JSON.toJSONString(this, SerializerFeature.PrettyFormat);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(TreeNode.parse("[1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]"));
     }
 }
