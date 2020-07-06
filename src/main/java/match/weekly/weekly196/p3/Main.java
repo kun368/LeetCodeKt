50c085863bd86884aa97f62ef88f9345c4c69fd4
kunwu <zhengkun.zzk@alibaba-inc.com>
2020-07-06 15:25:28 +0800

    蛋疼

--- src/main/java/match/weekly/weekly196/p3/Main.java
+++ src/main/java/match/weekly/weekly196/p3/Main.java
@@ -1,9 +1,23 @@
 package match.weekly.weekly196.p3;
 
 
+import utils.java.DebugUtils;
+
 class Solution {
     public int numSubmat(int[][] mat) {
-        return 0;
+        int N = mat.length, M = mat[0].length, ans = 0;
+        int[][] preSum = new int[N][M];
+        for (int i = 0; i < N; ++i) {
+            int s = 0;
+            for (int j = 0; j < M; ++j) {
+                preSum[i][j] = (s = mat[i][j] == 0 ? 0 : s + 1);
+                int cur = Integer.MAX_VALUE;
+                for (int k = i; k >= 0; --k) {
+                    ans += (cur = Math.min(cur, preSum[k][j]));
+                }
+            }
+        }
+        return ans;
     }
 }
 
@@ -13,6 +27,13 @@ class Solution {
  */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(new Solution().numSubmat(DebugUtils.parseIntArr2("[[1,0,1],\n" +
+                "            [1,1,0],\n" +
+                "            [1,1,0]]")));
+        System.out.println(new Solution().numSubmat(DebugUtils.parseIntArr2("[[0,1,1,0],\n" +
+                "            [0,1,1,1],\n" +
+                "            [1,1,1,0]]")));
+        System.out.println(new Solution().numSubmat(DebugUtils.parseIntArr2("[[1,1,1,1,1,1]]")));
+        System.out.println(new Solution().numSubmat(DebugUtils.parseIntArr2("[[1,0,1],[0,1,0],[1,0,1]]")));
     }
 }
