be6552c8259dfcf1c64c6fc4b2c8303c8526f047
kun368 <kun368@126.com>
2020-07-19 16:17:59 +0800

    take your time.

--- src/main/java/match/weekly/weekly198/p3/Main.java
+++ src/main/java/match/weekly/weekly198/p3/Main.java
@@ -1,7 +1,106 @@
 package match.weekly.weekly198.p3;
 
+import lombok.ToString;
+
+import java.util.*;
+
+class Solution {
+
+    @ToString
+    static class Segment {
+        int L, R;
+        String s;
+
+        public Segment(int l, int r, String s) {
+            L = l;
+            R = r;
+            this.s = s;
+        }
+
+        public int len() {
+            return s.length();
+        }
+    }
+
+    @ToString
+    static class DpNode implements Comparable<DpNode> {
+
+        int cnt, totLen, curNode, preNode;
+
+        public DpNode(int cnt, int totLen, int curNode, int preNode) {
+            this.cnt = cnt;
+            this.totLen = totLen;
+            this.curNode = curNode;
+            this.preNode = preNode;
+        }
+
+        @Override
+        public int compareTo(DpNode o2) {
+            int t = -Integer.compare(this.cnt, o2.cnt);
+            return t != 0 ? t : Integer.compare(this.totLen, o2.totLen);
+        }
+    }
+
+    public List<String> maxNumOfSubstrings(String s) {
+        int[] left = new int[26], right = new int[26];
+        for (int i = 0; i < 26; ++i) {
+            left[i] = s.indexOf('a' + i);
+            right[i] = s.lastIndexOf('a' + i);
+        }
+        List<Segment> segments = new ArrayList<>();
+        for (int i = 0; i < 26; ++i) {
+            int st = left[i], ed = right[i];
+            if (st < 0) continue;
+            boolean ok = true;
+            for (int j = st; j < s.length() && j <= ed; ++j) {
+                int cleft = left[s.charAt(j) - 'a'];
+                if (cleft < 0) continue;
+                if (cleft < st) {
+                    ok = false;
+                    break;
+                }
+                ed = Math.max(ed, right[s.charAt(j) - 'a']);
+            }
+            if (ok) {
+                segments.add(new Segment(st, ed, s.substring(st, ed + 1)));
+            }
+        }
+        segments.sort(Comparator.comparingInt(o -> o.R));
+
+        int N = segments.size();
+        DpNode[] dp = new DpNode[N];
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (segments.get(j).R >= segments.get(i).L) {
+                    continue;
+                }
+                DpNode newNode = new DpNode(1 + dp[j].cnt, segments.get(i).len() + dp[j].totLen, i, j);
+                if (dp[i] == null || newNode.compareTo(dp[i]) < 0) {
+                    dp[i] = newNode;
+                }
+            }
+            if (dp[i] == null) {
+                dp[i] = new DpNode(1, segments.get(i).len(), i, -1);
+            }
+        }
+        DpNode bestNode = Collections.min(Arrays.asList(dp));
+        List<String> retList = new ArrayList<>();
+        while (bestNode != null) {
+            retList.add(segments.get(bestNode.curNode).s);
+            if (bestNode.preNode >= 0) {
+                bestNode = dp[bestNode.preNode];
+            } else {
+                break;
+            }
+        }
+        return retList;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(new Solution().maxNumOfSubstrings("adefaddaccc"));
+        System.out.println(new Solution().maxNumOfSubstrings("abbaccd"));
+        System.out.println(new Solution().maxNumOfSubstrings("abab"));
     }
 }
--- src/main/java/match/weekly/weekly198/p4/Main.java
+++ src/main/java/match/weekly/weekly198/p4/Main.java
@@ -1,7 +1,53 @@
 package match.weekly.weekly198.p4;
 
+class Solution {
+
+    /**
+     * 计算区间的And值，左闭右开
+     */
+    private int spanAnd(int[][] pcnt, int l, int r) {
+        int ans = 0;
+        for (int t = 0; t < 32; ++t) {
+            if (pcnt[r][t] - pcnt[l][t] == r - l) {
+                ans |= (1 << t);
+            }
+        }
+        return ans;
+    }
+
+    public int closestToTarget(int[] arr, int target) {
+        int N = arr.length;
+        int[][] pcnt = new int[N + 1][32];
+        for (int i = 0; i < N; ++i) {
+            int c = arr[i];
+            for (int j = 0; j < 32; ++j) {
+                pcnt[i + 1][j] = pcnt[i][j] + ((c & (1 << j)) > 0 ? 1 : 0);
+            }
+        }
+        int ans = Integer.MAX_VALUE / 2;
+        for (int i = 0; i < N; ++i) {
+            int L = i, R = N;
+            while (L < R) {
+                int mid = (L + R) / 2;
+                int t = spanAnd(pcnt, i, mid + 1);
+                ans = Math.min(ans, Math.abs(t - target));
+                if (t == target) {
+                    break;
+                } else if (t < target) {
+                    R = mid;
+                } else {
+                    L = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println(new Solution().closestToTarget(new int[]{9, 12, 3, 7, 15}, 5));
+        System.out.println(new Solution().closestToTarget(new int[]{1000000, 1000000, 1000000}, 1));
+        System.out.println(new Solution().closestToTarget(new int[]{1, 2, 4, 8, 16}, 0));
     }
 }
