2e76500d8be0a1693512053f74141530627c953c
kunwu <zhengkun.zzk@alibaba-inc.com>
2020-07-31 09:16:17 +0800

    done

---
+++ src/main/java/interview/p08_03/Main.java
@@ -0,0 +1,19 @@
+package interview.p08_03;
+
+class Solution {
+    public int findMagicIndex(int[] nums) {
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
