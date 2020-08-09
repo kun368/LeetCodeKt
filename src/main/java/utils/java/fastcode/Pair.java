2681eacad8549e63ac29f3efe859ea5248200975
kun368 <kun368@126.com>
2020-08-09 11:50:18 +0800

    好用的数据结构

---
+++ src/main/java/utils/java/fastcode/Pair.java
@@ -0,0 +1,45 @@
+package utils.java.fastcode;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+@SuppressWarnings("unchecked")
+public class Pair<K, V> implements Comparable<Pair<K, V>> {
+
+    public K L;
+    public V R;
+
+    public Pair(K l, V r) {
+        L = l;
+        R = r;
+    }
+
+    private <E> int compare(E a, E b) {
+        return ((Comparable<? super E>) a).compareTo(b);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(L, pair.L) && Objects.equals(R, pair.R);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(L, R);
+    }
+
+    @Override
+    public int compareTo(@NotNull Pair<K, V> o) {
+        int t = compare(this.L, o.L);
+        return t == 0 ? compare(this.R, o.R) : t;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + L + ", " + R + ")";
+    }
+}
