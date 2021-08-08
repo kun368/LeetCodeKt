748665101141024f726d3ebb059b2cf082d8b331
kun368 <kun368@126.com>
2021-08-08 14:45:45 +0800

    ...

--- src/main/java/match/biweekly/biweekly58/p3/Solution.java
+++ src/main/java/match/biweekly/biweekly58/p3/Solution.java
@@ -1,9 +1,30 @@
 package match.biweekly.biweekly58.p3;
 
+import java.util.Arrays;
+
 class Solution {
 
     public int minSpaceWastedKResizing(int[] nums, int k) {
-        return 0;
+        int N = nums.length;
+        int[][] dp = new int[N + 1][k + 1];
+        int max = 0, sum = 0;
+        Arrays.fill(dp[0], 0);
+        for (int i = 0; i < N; ++i) {
+            max = Math.max(max, nums[i]);
+            sum += nums[i];
+            Arrays.fill(dp[i + 1], max * (i + 1) - sum);
+        }
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < k; ++j) {
+                int tmax = 0, tsum = 0;
+                for (int t = i + 1; t < N; ++t) {
+                    tmax = Math.max(tmax, nums[t]);
+                    tsum += nums[t];
+                    dp[t + 1][j + 1] = Math.min(dp[t + 1][j + 1], dp[i + 1][j] + tmax * (t - i) - tsum);
+                }
+            }
+        }
+        return dp[N][k];
     }
 
 
