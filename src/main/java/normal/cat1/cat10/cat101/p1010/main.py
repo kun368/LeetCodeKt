a0a502ed85dea1131df4d1d6be978cbce33b31a3
kun368 <kun368@126.com>
2023-05-07 09:10:33 +0800

    update daily code

--- src/main/java/normal/cat1/cat10/cat101/p1010/main.py
+++ src/main/java/normal/cat1/cat10/cat101/p1010/main.py
@@ -3,14 +3,14 @@ from utils.python.predef import *
 
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        counter = [0]*60
+        counter = [0] * 60
         for i in time:
             counter[i % 60] += 1
         ans = 0
         ans += counter[0] * (counter[0] - 1) // 2
         ans += counter[30] * (counter[30] - 1) // 2
         for i in range(1, 30):
-            ans += counter[i] * counter[60-i]
+            ans += counter[i] * counter[60 - i]
         return ans
 
 
