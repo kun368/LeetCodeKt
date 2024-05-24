39adb24de5bafa741b662866c8b216311390ed9f
kun368 <kun368@126.com>
2024-05-24 21:41:28 +0800

    update 1

--- src/main/java/match/weekly/weekly398/p1.py
+++ src/main/java/match/weekly/weekly398/p1.py
@@ -4,7 +4,7 @@ from utils.python.predef import *
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
         for i in range(1, len(nums)):
-            if nums[i] % 2 == nums[i-1] % 2:
+            if nums[i] % 2 == nums[i - 1] % 2:
                 return False
         return True
 
